df71cf725eaa88d9bdf251a9b23c66b6fb332719
Nara <shresthajeesan@gmail.com>
2026-03-24 16:17:02 +0545

    initial

---
+++ src/main/java/com/game/SoloFighter/SoloFighterApplication.java
@@ -0,0 +1,13 @@
+package com.game.SoloFighter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloFighterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloFighterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/SoloFighter/SoloFighterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.SoloFighter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoloFighterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
