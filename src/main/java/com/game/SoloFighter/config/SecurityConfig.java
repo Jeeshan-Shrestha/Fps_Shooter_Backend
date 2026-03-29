9292f999c95acbd02c5da2bbc0d3cc04e55f4a8f
Nara <shresthajeesan@gmail.com>
2026-03-29 13:16:51 +0545

    add beans authentication provider and manager

--- src/main/java/com/game/SoloFighter/config/SecurityConfig.java
+++ src/main/java/com/game/SoloFighter/config/SecurityConfig.java
@@ -1,13 +1,26 @@
 package com.game.SoloFighter.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import com.game.SoloFighter.services.MyUserDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig {
+
+    private final MyUserDetailsService myUserDetailsService;
+
+    public SecurityConfig(MyUserDetailsService myUserDetailsService){
+        this.myUserDetailsService = myUserDetailsService;
+    }
     
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http){
@@ -18,5 +31,15 @@ public class SecurityConfig {
         .build();
     }
 
-    
+    @Bean
+    AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider(myUserDetailsService);
+        provider.setPasswordEncoder(new BCryptPasswordEncoder(12));
+        return provider;
+    } 
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config){
+        return config.getAuthenticationManager();
+    }
 }
